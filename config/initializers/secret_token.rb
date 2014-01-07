9eb341dbb2bc81b0e88ce5fc9a3a3d7d851e6abb
Derrick Reimer <derrickreimer@gmail.com>
2014-01-06 20:11:38 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flutter::Application.config.secret_key_base = 'e320f0dfa37f37fecd33291429aff52be6dffa62783a642de5bd175c1ea0b028eb38c4892171d9e919c029fa58901e211e6d665f790d1263e65a04597384e2bf'
