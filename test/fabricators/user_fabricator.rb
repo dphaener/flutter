c28ccd9ccfd4d2ab5926d33c0d3a60cfb1a40fbb
Derrick Reimer <derrickreimer@gmail.com>
2014-01-13 19:32:38 -0800

    Add user validations

--- test/fabricators/user_fabricator.rb
+++ test/fabricators/user_fabricator.rb
@@ -1,6 +1,16 @@
 Fabricator(:user) do
-  email "derrick@example.com"
-  screen_name "derrickreimer"
+  email do 
+    sequence(:email) do |i|
+      "derrick#{i}@example.com"
+    end
+  end
+
+  screen_name do
+    sequence(:screen_name) do |i|
+      "derrickreimer#{i}"
+    end
+  end
+
   full_name "Derrick Reimer"
   url "http://example.com"
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should "require an email address" do
+    user = Fabricate.build(:user)
+    user.email = nil
+    assert user.invalid?
+  end
+
+  should "require a unique email address" do
+    user = Fabricate(:user)
+    user2 = Fabricate.build(:user)
+    user2.email = user.email
+    assert user2.invalid?
+  end
 end
