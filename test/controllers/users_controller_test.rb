a3ef6c077aef19ff45ee889652f8f412ad301de9
Derrick Reimer <derrickreimer@gmail.com>
2014-01-22 18:45:56 -0800

    Add registration form tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -9,4 +9,29 @@ class UsersControllerTest < ActionController::TestCase
     should respond_with(200)
     should render_template("new")
   end
+
+  context "POST #create" do
+    context "with valid input" do
+      setup do
+        @user_params = Fabricate.attributes_for(:user)
+      end
+
+      should "create a new user and redirect with a flash" do
+        assert_difference("User.count") do
+          post :create, :user => @user_params
+        end
+
+        assert_redirected_to(statuses_url)
+        assert_not_nil(flash[:notice])
+      end
+    end
+
+    context "with invalid input" do
+      setup do
+        post :create, :user => { :email => "foo" }
+      end
+
+      should render_template("new")
+    end
+  end
 end
