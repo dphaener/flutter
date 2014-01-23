7948f99095fc296db3990f7e3d556971119e7928
Derrick Reimer <derrickreimer@gmail.com>
2014-01-22 21:04:41 -0800

    Wire up login navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user, :logged_in?
+
   def login_as(user)
     session[:user_id] = user.id
   end
