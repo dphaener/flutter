ab7e211660d36c7b5f22967fe1c218997d8dae70
Darin Haener <dphaener@gmail.com>
2014-01-29 19:13:32 -0800

    Added new status form

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,5 +1,26 @@
 class StatusesController < ApplicationController
+  
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @statuses = Status.all.order("created_at DESC")
+  end
+
+  def new
+    @status = Status.new
+  end
+
+  def create
+    if current_user.statuses.create(status_params)
+      redirect_to statuses_url
+    else
+      render "new"
+    end
+  end
+
+private
+
+  def status_params
+    params.require(:status).permit(:text)
   end
 end
--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -6,10 +6,65 @@ class StatusesControllerTest < ActionController::TestCase
       get :index
     end
 
-    should respond_with(200)
     should render_template("index")
     should "assign to statuses" do
       assert_not_nil assigns(:statuses)
     end
+  end
+
+  context "GET #new" do
+    context "if user is not logged in" do
+      setup do
+        get :new
+      end
+
+      should "redirect to new session page" do
+        assert_redirected_to new_session_url
+      end
+    end
+    
+    context "if user is logged in" do
+      setup do
+        @user = Fabricate(:user)
+        login_as(@user)
+        get :new
+      end
+
+      should respond_with(200)
+      should render_template("new")
+      should "assign to statuses" do
+        assert_not_nil assigns(:status)
+      end
+    end
+    
+  end
+
+  context "POST #create" do
+    context "if user is not logged in" do
+      setup do
+        @status = Fabricate.attributes_for(:status)
+        post :create, status: @status
+      end
+
+      should "redirect to login page" do
+        assert_redirected_to new_session_url
+      end
+    end
+
+    context "if user is logged in" do
+      setup do
+        @user = Fabricate(:user)
+        login_as(@user)
+        @status = Fabricate.attributes_for(:status)
+      end
+
+      should "create a new status" do
+        assert_difference("Status.count") do
+          post :create, status: @status
+        end
+        assert_redirected_to statuses_url
+      end
+    end
+    
   end
 end
